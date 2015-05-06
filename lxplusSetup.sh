044b194726caa2024c5b9c9624ac234851403b6c
xju2 <xiangyang.ju@gmail.com>
2015-05-06 12:58:56 +0200

    up

--- lxplusSetup.sh
+++ lxplusSetup.sh
@@ -7,8 +7,6 @@ if [ ! -d $WSDIR/obj ]; then mkdir $WSDIR/obj; fi
 if [ ! -d $WSDIR/lib ]; then mkdir $WSDIR/lib; fi
 if [ ! -d "$WSDIR/test-bin" ]; then mkdir "$WSDIR/test-bin"; fi
 if [ ! -d "$WSDIR/bin" ]; then mkdir "$WSDIR/test-bin"; fi
-if [ ! -d $WSDIR/share ]; then mkdir $WSDIR/share; fi
-if [ ! -d $WSDIR/intermediates ]; then mkdir $WSDIR/intermediates; fi
 
 . /afs/cern.ch/atlas/project/HSG7/root/current/x86_64-slc6-gcc48/setup.sh 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
